9e2431dd49e7005cc0c41eff4e4cac7931d56235
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-06-07 16:29:20 +0300

    Настройка back

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,6 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -12,11 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
   const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await app.listen(port, () => {
+    console.log(`Сервер запущен по адресу: http://localhost:${port}/${globalPrefix}`);
+  });
 }
 
-bootstrap();
+bootstrap().catch(console.error);
