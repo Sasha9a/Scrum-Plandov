4d13fdf9f35547b0de49ffdf38daf34eea509c3f
adovgiy <adovgiy@zaoblako.ru>
2022-10-19 18:39:12 +0300

    websocket task

--- apps/web/src/app/modules/task/components/task/add/task-add.component.ts
+++ apps/web/src/app/modules/task/components/task/add/task-add.component.ts
@@ -1,23 +1,23 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { BoardDto } from "@scrum/shared/dtos/board/board.dto";
-import { SprintDto } from "@scrum/shared/dtos/sprint/sprint.dto";
-import { TaskDto } from "@scrum/shared/dtos/task/task.dto";
-import { TaskFormDto } from "@scrum/shared/dtos/task/task.form.dto";
-import { UserDto } from "@scrum/shared/dtos/user/user.dto";
-import { BoardService } from "@scrum/web/core/services/board/board.service";
-import { ErrorService } from "@scrum/web/core/services/error.service";
-import { SprintService } from "@scrum/web/core/services/sprint/sprint.service";
-import { TaskService } from "@scrum/web/core/services/task/task.service";
-import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
-import { forkJoin } from "rxjs";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { BoardDto } from '@scrum/shared/dtos/board/board.dto';
+import { SprintDto } from '@scrum/shared/dtos/sprint/sprint.dto';
+import { TaskFormDto } from '@scrum/shared/dtos/task/task.form.dto';
+import { UserDto } from '@scrum/shared/dtos/user/user.dto';
+import { BoardService } from '@scrum/web/core/services/board/board.service';
+import { ErrorService } from '@scrum/web/core/services/error.service';
+import { SprintService } from '@scrum/web/core/services/sprint/sprint.service';
+import { TaskService } from '@scrum/web/core/services/task/task.service';
+import { WebsocketTaskService } from '@scrum/web/core/services/task/websocket-task.service';
+import { AuthService } from '@scrum/web/core/services/user/auth.service';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'grace-task-add',
   templateUrl: './task-add.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaskAddComponent implements OnInit {
-
+export class TaskAddComponent implements OnInit, OnDestroy {
   public board: BoardDto;
   public sprint: SprintDto;
   public saving = false;
@@ -25,18 +25,24 @@ export class TaskAddComponent implements OnInit {
   public users: UserDto[];
   public sprints: SprintDto[];
 
-  public constructor(private readonly config: DynamicDialogConfig,
-                     private readonly sprintService: SprintService,
-                     private readonly taskService: TaskService,
-                     private readonly boardService: BoardService,
-                     private readonly cdRef: ChangeDetectorRef,
-                     private readonly errorService: ErrorService,
-                     private readonly ref: DynamicDialogRef) {
+  public constructor(
+    private readonly config: DynamicDialogConfig,
+    private readonly sprintService: SprintService,
+    private readonly websocketTaskService: WebsocketTaskService,
+    private readonly authService: AuthService,
+    private readonly taskService: TaskService,
+    private readonly boardService: BoardService,
+    private readonly cdRef: ChangeDetectorRef,
+    private readonly errorService: ErrorService,
+    private readonly ref: DynamicDialogRef
+  ) {
     this.board = config.data.board;
     this.sprint = config.data.sprint;
   }
 
   public ngOnInit(): void {
+    this.websocketTaskService.createWSConnection(this.authService.getToken(), this.board?._id);
+
     forkJoin([
       this.boardService.findAllUsersByBoard(this.board?._id),
       this.sprintService.findAllByBoardDropdown(this.board?._id)
@@ -47,11 +53,15 @@ export class TaskAddComponent implements OnInit {
     });
   }
 
+  public ngOnDestroy() {
+    this.websocketTaskService.socket?.disconnect();
+  }
+
   public create(body: TaskFormDto) {
     this.saving = true;
     this.cdRef.markForCheck();
 
-    this.taskService.create<TaskFormDto, TaskDto>(body).subscribe({
+    this.websocketTaskService.createTask({ boardId: this.board?._id, body }).subscribe({
       next: (task) => {
         this.saving = false;
         this.cdRef.markForCheck();
@@ -64,5 +74,4 @@ export class TaskAddComponent implements OnInit {
       }
     });
   }
-
 }
--- apps/web/src/app/modules/task/components/task/edit/task-edit.component.ts
+++ apps/web/src/app/modules/task/components/task/edit/task-edit.component.ts
@@ -1,23 +1,24 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { SprintDto } from "@scrum/shared/dtos/sprint/sprint.dto";
-import { TaskDto } from "@scrum/shared/dtos/task/task.dto";
-import { TaskFormDto } from "@scrum/shared/dtos/task/task.form.dto";
-import { UserDto } from "@scrum/shared/dtos/user/user.dto";
-import { BoardService } from "@scrum/web/core/services/board/board.service";
-import { ConfirmDialogService } from "@scrum/web/core/services/confirm-dialog.service";
-import { ErrorService } from "@scrum/web/core/services/error.service";
-import { SprintService } from "@scrum/web/core/services/sprint/sprint.service";
-import { TaskService } from "@scrum/web/core/services/task/task.service";
-import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
-import { forkJoin } from "rxjs";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { SprintDto } from '@scrum/shared/dtos/sprint/sprint.dto';
+import { TaskDto } from '@scrum/shared/dtos/task/task.dto';
+import { TaskFormDto } from '@scrum/shared/dtos/task/task.form.dto';
+import { UserDto } from '@scrum/shared/dtos/user/user.dto';
+import { BoardService } from '@scrum/web/core/services/board/board.service';
+import { ConfirmDialogService } from '@scrum/web/core/services/confirm-dialog.service';
+import { ErrorService } from '@scrum/web/core/services/error.service';
+import { SprintService } from '@scrum/web/core/services/sprint/sprint.service';
+import { TaskService } from '@scrum/web/core/services/task/task.service';
+import { WebsocketTaskService } from '@scrum/web/core/services/task/websocket-task.service';
+import { AuthService } from '@scrum/web/core/services/user/auth.service';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'grace-task-edit',
   templateUrl: './task-edit.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaskEditComponent implements OnInit {
-
+export class TaskEditComponent implements OnInit, OnDestroy {
   public taskId: string;
   public task: TaskDto;
   public loading = true;
@@ -25,20 +26,25 @@ export class TaskEditComponent implements OnInit {
   public users: UserDto[];
   public sprints: SprintDto[];
 
-  public constructor(private readonly config: DynamicDialogConfig,
-                     private readonly taskService: TaskService,
-                     private readonly sprintService: SprintService,
-                     private readonly boardService: BoardService,
-                     private readonly cdRef: ChangeDetectorRef,
-                     private readonly errorService: ErrorService,
-                     private readonly ref: DynamicDialogRef,
-                     private readonly confirmService: ConfirmDialogService) {
+  public constructor(
+    private readonly config: DynamicDialogConfig,
+    private readonly taskService: TaskService,
+    private readonly sprintService: SprintService,
+    private readonly websocketTaskService: WebsocketTaskService,
+    private readonly authService: AuthService,
+    private readonly boardService: BoardService,
+    private readonly cdRef: ChangeDetectorRef,
+    private readonly errorService: ErrorService,
+    private readonly ref: DynamicDialogRef,
+    private readonly confirmService: ConfirmDialogService
+  ) {
     this.taskId = config.data.taskId;
   }
 
   public ngOnInit(): void {
     this.taskService.findById<TaskDto>(this.taskId).subscribe((task) => {
       this.task = task;
+      this.websocketTaskService.createWSConnection(this.authService.getToken(), this.task?.board?._id);
       forkJoin([
         this.boardService.findAllUsersByBoard(this.task?.board?._id),
         this.sprintService.findAllByBoardDropdown(this.task?.board?._id)
@@ -52,11 +58,15 @@ export class TaskEditComponent implements OnInit {
     });
   }
 
+  public ngOnDestroy() {
+    this.websocketTaskService.socket?.disconnect();
+  }
+
   public edit(body: TaskFormDto) {
     this.loading = true;
     this.cdRef.markForCheck();
 
-    this.taskService.update<TaskFormDto, TaskDto>(this.taskId, body).subscribe({
+    this.websocketTaskService.updateTask({ taskId: this.taskId, boardId: this.task?.board?._id, body }).subscribe({
       next: (task) => {
         this.loading = false;
         this.cdRef.markForCheck();
@@ -86,5 +96,4 @@ export class TaskEditComponent implements OnInit {
       }
     });
   }
-
 }
